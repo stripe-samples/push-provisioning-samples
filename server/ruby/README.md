8de98ed94d2bf43ba351618d9641d1f8c8be8e9c
Vlad Chernis <vbc@stripe.com>
2023-11-27 14:14:50 -0800

    backend initial commit

---
+++ server/ruby/README.md
@@ -0,0 +1,62 @@
+Example App Backend
+====
+
+This is a [Sinatra](http://www.sinatrarb.com/) webapp that you can use to test Stripe's example push provisioning apps.
+
+This is intended for example purposes--it is not a production-ready implementation.
+
+To deploy this for free on Glitch, visit https://glitch.com/edit/#!/stripe-push-provisioning-example-backend and click "Remix".
+
+In your `.env` file in Glitch, set `STRIPE_LIVE_SECRET_KEY` to your secret key. Find this at https://dashboard.stripe.com/apikeys (it'll look like `sk_live_****`). Then set `CARDHOLDER_ID`, which you can find at https://dashboard.stripe.com/issuing/cardholders (it'll look like `ich_...`).
+
+Then, set `SAMPLE_PP_BACKEND_URL` in the example iOS/Android app to your Glitch Remix URL (it'll be in the format https://my-example-app.glitch.me).
+
+Happy testing!
+
+How to run locally
+----
+
+Follow the steps below to run locally.
+
+0. Prerequisites
+
+You will need a Stripe account in order to run the sample backend. Once you set up your account
+and completed the [steps to get Issuing access](https://stripe.com/docs/issuing/cards/digital-wallets#request-access),
+go to the Stripe developer dashboard to find your [API keys](https://dashboard.stripe.com/apikeys) and [cardholder ID](https://dashboard.stripe.com/issuing/cardholders).
+
+To configure the app, copy the `.env.example` file into a file named `.env`:
+
+```
+cp .env.example .env
+```
+Fill in `.env` with your configuration:
+
+```
+# See https://dashboard.stripe.com/apikeys
+STRIPE_LIVE_SECRET_KEY=sk_live_...
+# See https://dashboard.stripe.com/issuing/cardholders
+CARDHOLDER_ID=ich_...
+# These should match your client app config
+USERNAME=...
+PASSWORD=...
+```
+
+1. Install dependencies
+```
+RUBY_CFLAGS=-DUSE_FFI_CLOSURE_ALLOC # flag to build ruby 2.5.8 on M1 Macs \
+  rbenv install 2.5.8
+rbenv local 2.5.8
+gem install bundler -v 2.3.26
+bundle install
+```
+
+2. Run the application
+```
+bundle exec ruby server.rb
+```
+
+Basic smoke test
+---
+```
+curl -u cardholder:password --basic http://localhost:4242/cards
+```
---
+++ server/ruby/server.rb
@@ -0,0 +1,117 @@
+require 'sinatra'
+require 'stripe'
+require 'dotenv'
+require 'json'
+require 'encrypted_cookie'
+
+# Replace if using a different env file or config
+# Copy the .env.example in the root into a .env file in this folder.
+Dotenv.load
+
+# For sample support and debugging, not required for production:
+Stripe.set_app_info(
+  'stripe-samples/push-provisioning',
+  version: '0.0.1',
+  url: 'https://github.com/stripe-samples/push-provisioning'
+)
+Stripe.api_key = ENV['STRIPE_LIVE_SECRET_KEY']
+
+set :port, 4242
+
+# Listens to all network interfaces. This allows any device on your
+# network to connect.
+set :bind, '0.0.0.0'
+
+$stdout.sync = true # Get puts to show up in heroku logs
+
+use Rack::Session::EncryptedCookie,
+    secret: 'replace_me_with_a_real_secret_key' # Actually use something secret here!
+
+get '/' do
+  status 200
+  return log_info('Great, your backend is set up. Now you can configure the Stripe example apps to point here.')
+end
+
+# Given an authenticated user, look up their corresponding cardholder ID to get a list of the user's cards.
+# See https://stripe.com/docs/api/issuing/cards/list
+get '/cards' do
+  authenticate!
+  begin
+    cards_response = Stripe::Issuing::Card.list(
+      {
+        cardholder: authenticated_cardholder_id,
+        limit: 10
+      }
+    )
+    sanitized_cards = cards_response.data.map do |card|
+      {
+        id: card.id,
+        last4: card.last4,
+        brand: card.brand,
+        cardholder_name: card.cardholder.name,
+        eligible_for_google_pay: card.status == 'active' && card.wallets.google_pay.eligible,
+        eligible_for_apple_pay: card.status == 'active' && card.wallets.apple_pay.eligible,
+        primary_account_identifier: card.wallets.primary_account_identifier   # nullable
+      }
+    end
+  rescue KeyError, Stripe::StripeError => e
+    status 404
+    return log_info("Error listing cards: #{e.message}")
+  end
+
+  content_type :json
+  status 200
+  # TODO: Warn users against caching PAI, cache can be stale for the initial value prior to the first provision.
+  {data: sanitized_cards}.to_json
+end
+
+# Create an ephemeral key for the given card ID.
+# See https://stripe.com/docs/issuing/cards/digital-wallets?platform=iOS#update-your-backend
+# See https://stripe.com/docs/issuing/cards/digital-wallets?platform=Android#update-your-backend
+post '/ephemeral_keys' do
+  authenticate!
+  begin
+    # TODO: Ideally the ephemeral key supports only certain operations (e.g. a key meant for push provisioning can't
+    #   be used to change the pin).
+    key = Stripe::EphemeralKey.create(
+      { issuing_card: params['card_id'] },
+      { stripe_version: params['api_version'] }
+    )
+  rescue Stripe::StripeError => e
+    status 402
+    return log_info("Error creating ephemeral key: #{e.message}")
+  end
+
+  content_type :json
+  status 200
+  key.to_json
+end
+
+helpers do
+  def authenticate!
+    return if authenticated?
+
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authenticated?
+    @auth ||= Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and !authenticated_cardholder_id.nil?
+    # TODO: show 2FA check (potentially for a subset of endpoints), more relevant for apple
+  end
+
+  # A more realistic version of this may involve using the authenticated user to look up a corresponding cardholder ID.
+  # See https://stripe.com/docs/issuing/cards#create-cardholder
+  def authenticated_cardholder_id
+    cardholder_db = {
+      [ENV["USERNAME"], ENV['PASSWORD']] => ENV['CARDHOLDER_ID']
+    }
+    cardholder_db[@auth.credentials]
+  end
+
+  def log_info(message)
+    puts "\n#{message}\n\n"
+    message
+  end
+end
